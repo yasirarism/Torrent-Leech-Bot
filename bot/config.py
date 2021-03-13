07efe72f402e6b0423d4305bee34e2184ae0bb72
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-03-13 19:04:23 +0700

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,12 +2,12 @@ from bot.sample_config import Config
 #Fill your all data, read readme for reference
 
 class Config(Config):
-	TG_BOT_TOKEN= "1123456:jff7ftff7mftdfjk5Bi1t4YYdkQ" # get a token from @BotFather
-	APP_ID = 123450  # Get these values from my.telegram.org
-	API_HASH = "524ff7f5k0b2g6gv6e8347a38"  # Get these values from my.telegram.org
-	OWNER_ID = [1204927413] #ID of bot owner (sapreated with space)
-	AUTH_CHANNEL = [-1001401904278] #Sapreated with space.
-	BOT_USERNAME = "@UVideoCompresserbot" #bot username with @
+	TG_BOT_TOKEN= "1272317072:AAEjptqoLo3SQxKmBDu-AHa7mgaZrsF6VLo" # get a token from @BotFather
+	APP_ID = 1046625  # Get these values from my.telegram.org
+	API_HASH = "c68afc924b92d73ce27708b155f1e5b4"  # Get these values from my.telegram.org
+	OWNER_ID = [617426792] #ID of bot owner (sapreated with space)
+	AUTH_CHANNEL = [-1001455886928] #Sapreated with space.
+	BOT_USERNAME = "@VioletAssistantBot" #bot username with @
 #Opitional filds(Leave this if you dont know what is this.......)
 	FINISHED_PROGRESS_STR = ""
 	UN_FINISHED_PROGRESS_STR = ""
